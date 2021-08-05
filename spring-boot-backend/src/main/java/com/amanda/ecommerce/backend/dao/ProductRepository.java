be7346e1b3f5f4a1578766c10041049079783a23
Amanda R <amanda@Amandas-MacBook-Pro.local>
2021-08-05 18:06:20 -0400

    completed lecture 62

--- spring-boot-backend/src/main/java/com/amanda/ecommerce/backend/dao/ProductRepository.java
+++ spring-boot-backend/src/main/java/com/amanda/ecommerce/backend/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.amanda.ecommerce.backend.dao;
 
 import com.amanda.ecommerce.backend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
