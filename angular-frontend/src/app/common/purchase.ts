5bf5a1ce81e3e39b83fe50b35662bf074ce5405e
Amanda <amanda@Amandas-iMac.local>
2021-09-18 23:58:17 -0400

    finished lecture 211

---
+++ angular-frontend/src/app/common/purchase.ts
@@ -0,0 +1,13 @@
+import { Address } from "./address";
+import { Customer } from "./customer";
+import { Order } from "./order";
+
+export class Purchase {
+    
+    customer!: Customer;
+    shippingAddress!: Address;
+    billingAddress!: Address;
+    order!: Order;
+    orderItems!: OrderITem[];
+    
+}
